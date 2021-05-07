bd31e8a725358dc75cf5caa215d0b1fde35c0234
GDI19 <dimon.1982@mail.ru>
2021-05-07 19:48:21 +0300

    update task 1

--- lesson_6/task_1.py
+++ lesson_6/task_1.py
@@ -13,12 +13,13 @@ step = 10
 
 
 def ascii_print_1(start, end, step):
-    memory_size = 0
+    memory_list = []
     while start < 127:
         if start == 122:
             step = 6
 
         result = ''
+        memory_size = 0
         for i in range(start, start + step):
             result = result + str(i) + '  ' + chr(i) + '  '
 
@@ -28,23 +29,26 @@ def ascii_print_1(start, end, step):
         size_res = sys.getsizeof(result)
         print(size_res)
         print('-' * len(result))
-        memory_size += size_res
+        memory_size += size_res + start + end + step
+        memory_list.append(memory_size)
 
-    memory_size = memory_size + start + end + step
+    return(memory_list)
 
-    print(memory_size) # 1451 bytes Python 3.8.3 windows 10 (64x)
-
-# ascii_print_1(start, end, step)
-
+# print(ascii_print_1(start, end, step))
+"""
+За время работы функци используется 1451 bytes,
+но т.к. строка перезаписывается то получается максимальный объем
+используется 388 байт Python 3.8.3 windows 10 (64x)"""
 # ---------------------------------------------------------------------
 
 def ascii_print_2(start, end, step):
-    memory_size = 0
+    memory_list = []
     while start < 127:
         if start == 122:
             step = 6
 
         result = []
+        memory_size = 0
         for i in range(start, start + step):
             result.append(str(i) + '  ' + chr(i) + '  ')
 
@@ -61,9 +65,52 @@ def ascii_print_2(start, end, step):
         print('-' * len(result))
         memory_size += size_res
 
-    memory_size = memory_size + start + end + step
+        memory_size = memory_size + start + end + step
+        memory_list.append(memory_size)
 
-    print(memory_size) # 7441 bytes Python 3.8.3 windows 10 (64x)
+    return (memory_list)
 
-# ascii_print_2(start, end, step)
+# print(ascii_print_2(start, end, step))
+"""
+за время функции используется 7441 bytes, но 
+т.к. список перезаписывается то максимальный размер 
+состовляет 1013 байт Python 3.8.3 windows 10 (64x)
+"""
+# ----------------------------------------------------------------------
+
+list_ascii = [chr(i) for i in range(0, 128)]
+def ascii_print_3(start, end, step):
+    count = 1
+    for i in range(start, end+1):
+        print(f'{i}  {list_ascii[i]}', end=' | ')
+        if count % step != 0:
+            count += 1
+        else:
+            print()
+            count += 1
+
+ascii_print_3(start, end, step)
+
+"""
+print(sys.getsizeof(list_ascii))
+total_size = 0
+for item in list_ascii:
+    size = sys.getsizeof(item)
+    total_size += size
+
+print(total_size)
+
+t_size = 0
+for i in range(127):
+    size = sys.getsizeof(i)
+    print(i, size )
+    t_size += size
+print(t_size)
+
+созданный список ASCII занимает 7640 bytes, Python 3.8.3 windows 10 (64x)
+функция лишь обращается к этим значениям
+
+Кроме этого в выше приведенных функциях не учитывался 
+цикл for i in range(32,127) займет 2660 байт = (28b * 95)
+"""
 
