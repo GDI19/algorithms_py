0ad7233a57834fce17fca73b4d88b9628a5bbe96
GDI19 <dimon.1982@mail.ru>
2021-05-09 20:12:08 +0300

    add task 1-2

---
+++ lesson_7/task_2.py
@@ -0,0 +1,32 @@
+from random import uniform
+
+array = [uniform(0, 50) for i in range(0, 10)]
+print(array)
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+    return merge(left, right, arr.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor +right_cursor ] =left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+print(merge_sort(array))
