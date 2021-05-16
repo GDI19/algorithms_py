60e8a2f914d10cfd2d4df29d081e5a28396f99a3
GDI19 <dimon.1982@mail.ru>
2021-05-16 19:25:39 +0300

    add task 1

--- lesson_8/hash.py
+++ lesson_8/hash.py
@@ -1,16 +1,32 @@
+"""
+Определение количества различных подстрок с использованием хеш-функции.
+Пусть на вход функции дана строка. Требуется вернуть количество различных
+подстрок в этой строке.
+Примечание: в сумму не включаем пустую строку и строку целиком.
+Пример работы функции:
+
+func("papa")
+6
+func("sova")
+9
+"""
+
 import hashlib
 
 
 def rabin_karp(s):
-    len_sub = [i + 1 for i in range(len(s))]
+    len_sub = [i + 1 for i in range(len(s) - 1)]
     h_list = []
+    count = 0
     for item in len_sub:
-        # h_subs = hashlib.sha1(s[:item].encode('utf-8')).hexdigest()
+        h_subs = hashlib.sha1(s[:item].encode('utf-8')).hexdigest()
         for i in range(len(s)):
             h_str = hashlib.sha1(s[i:i + item].encode('utf-8')).hexdigest()
             if h_str not in h_list:
                 print(s[i:i + item])
+                count += 1
             h_list.append(h_str)
+    return count
 
 
-rabin_karp('abcd')
+print(rabin_karp('sova'))
